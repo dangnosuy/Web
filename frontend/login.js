b4c12111c868f596430562e4a022c4c6baa8c6bf
TongXuanVu <tongxuanvuk1920@gmail.com>
2025-04-01 07:11:27 +0700

    Add files via upload

--- frontend/login.js
+++ frontend/login.js
@@ -15,25 +15,47 @@ $(document).ready(function()
     });
 });
 
-document.getElementById("username").addEventListener("input", function() {
-    document.getElementById("check_username").className = "form";
-});
-
-const email_form = document.getElementById("email")
-if(email_form){
-    email_form.addEventListener("input", function() {
-    document.getElementById("check_email").className = "form";
+const username_form = document.getElementById("username")
+if(username_form){
+    username_form.addEventListener("click", function() {
+        document.getElementById("username_form").className = "form";
 });
 }
 
-document.getElementById("password").addEventListener("input", function() {
-    document.getElementById("check_password").className = "form";
+const email_form = document.getElementById("email")
+if(email_form){
+    email_form.addEventListener("click", function() {
+        document.getElementById("email_form").className = "form";
 });
+}
+
+const password_form = document.getElementById("password")
+if(password_form){
+    password_form.addEventListener("click", function() {
+        document.getElementById("password_form").className = "form";
+});
+}
 
 const confirm_form = document.getElementById("confirm")
 if(confirm_form){
-    confirm_form.addEventListener("input", function() {
-    document.getElementById("check_confirm").className = "form";
+    confirm_form.addEventListener("click", function() {
+        document.getElementById("confirm_form").className = "form";
+});
+}
+
+const verify_form = document.getElementById("verify")
+if(verify_form){
+    verify_form.addEventListener("click", function() {
+        document.getElementById("verify_form").className = "form";
+});
+}
+
+const reset_password = document.getElementById("reset_password")
+if(reset_password){
+    reset_password.addEventListener("click", function() {
+        document.getElementById("SignIn_form").style.display = "none";
+        document.getElementById("ResetPassword_form").style.display = "block";
+        document.getElementById("return").style.display = "";
 });
 }
 
@@ -41,10 +63,11 @@ const SignUpButton = document.getElementById("sign_up");
 if (SignUpButton) {
     SignUpButton.addEventListener("click", function(event_up) {
         event_up.preventDefault();
-        Send_signup();
+        document.getElementById("error_message").textContent = ""
+        Send_SignUp();
     });
 }
-async function Send_signup() {
+async function Send_SignUp() {
     const username = document.getElementById("username").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
@@ -52,28 +75,36 @@ async function Send_signup() {
 
     if (!username.trim() || !email.trim() || !password.trim() || !confirm_password.trim())
         {
+            error_message = "Please enter ";
             if (!username.trim()) {
-                document.getElementById("check_username").className = "form_error";
+                document.getElementById("username_form").className = "form_error";
+                error_message += "Username, "
             }
             if (!email.trim()) {
-                document.getElementById("check_email").className = "form_error";
+                document.getElementById("email_form").className = "form_error";
+                error_message += "Email, "
             }
             if (!password.trim()) {
-                document.getElementById("check_password").className = "form_error";
+                document.getElementById("password_form").className = "form_error";
+                error_message += "Password, "
             }
             if (!confirm_password.trim()) {
-                document.getElementById("check_confirm").className = "form_error";
+                document.getElementById("confirm_form").className = "form_error";
+                error_message += "Confirm Password, "
             }
+            error_message = error_message.slice(0, -2) + "."
+            document.getElementById("error_message").textContent = error_message
             return
         }
     
         if (password != confirm_password) {
-            document.getElementById("check_confirm").className = "form_error";
+            document.getElementById("confirm_form").className = "form_error";
+            document.getElementById("password_form").className = "form_error";
+            document.getElementById("error_message").textContent = "Password and password confirmation don't match."
+            return
         }
 
-        const password_hash = sha256(password);
-        //const password_hash = await bcrypt.hash(password, 10)
-        const data = { username, email, password_hash};
+        const data = { username, email, password};
     try {
         const response = await fetch("http://127.0.0.1:5550/api/sign_up", {
         method: "POST",
@@ -82,17 +113,26 @@ async function Send_signup() {
         });
          
         const result = await response.json();
-        if (!response.ok) {
-            document.getElementById("check_username").className = "form_error";
-            document.getElementById("check_email").className = "form_error";
+        if (result.email_form || result.username_form) {
+            error_message = "";
+            if(result.username_form){
+                document.getElementById("username_form").className = "form_error";
+                error_message += "Username, "
+            }
+            if(result.email_form){
+                document.getElementById("email_form").className = "form_error";
+                error_message += "Email, "
+            }
+            error_message = error_message.slice(0, -2) + " has been used"
+            document.getElementById("error_message").textContent = error_message
+            return
         }
 
         if (result.success) {
-
-            alert("Đăng ký thành công!");
-            window.location.href = "./Sign_in.html";
+            document.getElementById("SignUp_form").style.display = "none";
+            document.getElementById("verify_email_form").style.display = "block";
         } else {
-            alert("Đăng ký thất bại: " + result.message);
+            alert("Registration Failed: " + result.message);
         }
     } catch (err) {
            alert(err.message);
@@ -103,26 +143,31 @@ const SignInButton = document.getElementById("sign_in");
 if (SignInButton) {
     SignInButton.addEventListener("click", function(event_in) {
         event_in.preventDefault();
-        Send_signin();
+        document.getElementById("error_message").textContent = ""
+        Send_SignIn();
     });
 }
-async function Send_signin() {
+async function Send_SignIn() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
     if(!username.trim() || !password.trim())
     {
+        error_message = "Please enter";
         if (!username.trim()) {
-            document.getElementById("check_username").className = "form_error";
+            document.getElementById("username_form").className = "form_error";
+            error_message += " Username,"
         }
         if (!password.trim()) {
-            document.getElementById("check_password").className = "form_error";
+            document.getElementById("password_form").className = "form_error";
+            error_message += " Password,"
         }
+        error_message = error_message.slice(0, -1) + "."
+        document.getElementById("error_message").textContent = error_message
         return
     }
 
-    const password_hash = sha256(password)
-    const data = {username, password_hash};
+    const data = {username, password};
     try {
         const response = await fetch("http://127.0.0.1:5550/api/sign_in", {
         method: "POST",
@@ -132,15 +177,112 @@ async function Send_signin() {
          
         const result = await response.json();
         if (!response.ok){
-            alert(result.error);
-            return;
+            document.getElementById("username_form").className = "form_error";
+            document.getElementById("password_form").className = "form_error";
+            document.getElementById("error_message").textContent = result.message;
+            return
         }
         if (result.success) {
             sessionStorage.setItem("loggedIn", "true");
             localStorage.setItem('username', username);
             window.location.href = "./index.html";
         } else {
-            alert("Đăng nhập thất bại: " + result.message);
+            alert("Login Failed: " + result.message);
+        }
+    } catch (err) {
+           alert(err.message);
+    }
+}
+
+const VerifyEmailButton = document.getElementById("verify_email");
+if (VerifyEmailButton ) {
+    VerifyEmailButton.addEventListener("click", function(event_in) {
+        event_in.preventDefault();
+        document.getElementById("error_message_verify").textContent = ""
+        Send_VerifyEmail();
+    });
+}
+async function Send_VerifyEmail() {
+    const email = document.getElementById("email").value;
+    const code = document.getElementById("verify").value;
+    
+    if(!code.trim()||!email.trim())
+    {
+        document.getElementById("verify_form").className = "form_error";
+        document.getElementById("error_message_verify").textContent = "Please enter the verification code";
+        return
+    }
+    try {
+        const response = await fetch("http://127.0.0.1:5550/api/verify_email", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, code }),
+        });
+
+        if (!response.ok) {
+            document.getElementById("verify_form").className = "form_error";
+            document.getElementById("error_message_verify").textContent = result.message;
+            return;
+        }
+
+        const result = await response.json();
+        if (result.success) {
+            window.location.href = "./Sign_in.html";
+        } else {
+            alert("Email Confirmation Failed: " + result.message);
+        }
+    } catch (err) {
+        alert(err.message);
+    }
+}
+
+const ResetPasswordButton = document.getElementById("submit_reset");
+if (ResetPasswordButton) {
+    document.getElementById("submit_reset").addEventListener("click", function(event_in) {
+        event_in.preventDefault();
+        Send_SubmitResetPassword();
+    });
+}
+async function Send_SubmitResetPassword() {
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+
+    if(!username.trim() || !email.trim())
+    {
+        error_message = "Please enter";
+        if (!username.trim()) {
+            document.getElementById("username_form").className = "form_error";
+            error_message += " Username,"
+        }
+        if (!email.trim()) {
+            document.getElementById("email_form").className = "form_error";
+            error_message += " Email,"
+        }
+        error_message = error_message.slice(0, -1) + "."
+        document.getElementById("error_message_verify").textContent = error_message
+        return
+    }
+
+    const data = {username, email};
+    try {
+        const response = await fetch("http://127.0.0.1:5550/api/sign_in", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data),
+        });
+         
+        const result = await response.json();
+        if (!response.ok){
+            document.getElementById("username_form").className = "form_error";
+            document.getElementById("email_form").className = "form_error";
+            document.getElementById("error_message").textContent = result.message;
+            return
+        }
+        if (result.success) {
+            // 
+            //
+        } else {
+            alert("Reset password Failed: " + result.message);
         }
     } catch (err) {
            alert(err.message);
